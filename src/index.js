63df3319526992bbd11dca88a1b72d0924f0b666
Elijah Seigel <esseigel7975@WIN2.CS.KSU.EDU>
2017-10-18 10:35:10 -0500

    up to date with class code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
-// index.js
-import Game from './game';
-import './game.css';
+import './index.css';
+import Terrain from './terrain';
+import Ship from './ship';
 
-/* Construct the game */
-const game = new Game();
+var canvas = document.createElement('canvas');
+canvas.width = 300;
+canvas.height = 300;
+var context = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+var terrain = new Terrain(300, 300);
+terrain.render(context);
+
+var ship = new Ship(300, 300);
+ship.render(context);
